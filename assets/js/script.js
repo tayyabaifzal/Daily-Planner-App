fc74b5ae19b5f9529ed26eda29c8391eb021d424
Tayyaba <tayyaba.afzal@gmail.com>
2024-01-28 15:13:29 +0000

    code for time block colour function added to js. This will display colous to show time as past, present and future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
 var saveBtn = $(".saveBtn");
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+function timeBlockColor(){
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currHour = parseInt($(this).attr("id"));
+
+        if (currHour > hour){
+            $(this).addClass("future");
+        } else if (currHour === hour){
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
