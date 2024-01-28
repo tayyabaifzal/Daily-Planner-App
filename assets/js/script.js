b9318a08da64b75186bf7dab89bb94ff7ddccc6a
Tayyaba <tayyaba.afzal@gmail.com>
2024-01-28 07:50:43 +0000

    html file modified. 9 to 5 hours calendar added to help plan a work day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
+// code3 
+
 $(document).ready(function() {
 
     $("#currentDay").text(dayjs().format("MMMM D, YYYY"));
 
 
-for (let hour = 17; hour<= 24; hour++){
+for (let hour = 9; hour<= 24; hour++){
     let row = $("<div>").addClass("row");
     let time = $("<div>")
        .addClass("hour")
@@ -22,33 +24,110 @@ if (dayjs().hour() > hour){
     description.addClass("future");
 }
 
+}})
 
-description.val(localStorage.getItem(hour));
-
-
-
-saveBtn.on("click", function () {
-    localStorage,setItem(hour, description.val());
-});
-
-
-row.append(time, description, saveBtn);
-
-
-$("#time-block").append(row);
-}
-}); 
+// description.val(localStorage.getItem(hour));
 
 
 
+// saveBtn.on("click", function () {
+//     localStorage,setItem(hour, description.val());
+// });
 
 
+// row.append(time, description, saveBtn);
 
 
+// $("#time-block").append(row);
+// }
+// }); 
 
 
+//code2
 
 
+// $(document).ready(function(){
+//     function hourUpdate (){
+//         var currentHour =moment().hours();
+
+//         $(".time-block").each(function(){
+//             var blockHour = parseInt($(this).attr("id").split("-")[1]);
+//             if (blockHour<currentHour){
+//                 $(this).addClass("past");
+//             } else if (blockHour === currentHour){
+//                 $(this).removeClass("past");
+//                 $(this).addClass("present");
+//             } else {
+//                 $(this).removeClass("past");
+//                 $(this).removeClass("present");
+//                 $(this).addClass("future");
+//             }
+//         })
+//     }
+//     hourUpdate();
+
+//     var interval = setInterval(hourUpdate,15000);
+
+//     $(".saveBtn").on("click",function(){
+//         var value = $(this).siblings(".description").val();
+//         var time = $(this).parent().attr("id");
+
+//         localStorage.setItem(time,value);
+//     })
+// for (var i=9; i<19;i++){
+//     $("#hour-"+i+" .description").val(localStorage.getItem("hour-"+i));
+// }
+//   $("#currentDay").text(moment().format("dddd, MMMM Do"))
+// });
+ 
+
+// // code1
+
+// add and remove class -- similar to hide
+// Include for past/pres./future
+// grab current hour from moment.js
+
+// Getting the date to show up - google
+// Use moment.js for the time
+// needed for the current date
+
+// var currentHour = moment().hour();
+// var militaryHour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+// var saveBtn = document.querySelectorAll(".saveBtn");
+// console.log(currentHour);
+
+
+// document.querySelector("#currentDay").textContent = moment().format("dddd, MMMM Do YYYY");
+// for (var i = 0; i < 9; i++) {
+//   if (currentHour === militaryHour[i]) {
+//     console.log(i);
+//     document.getElementById(i).classList.add("present");
+//   }
+//   if (currentHour > militaryHour[i]) {
+//     console.log(i);
+//     document.getElementById(i).classList.add("past");
+//   }
+//   if (currentHour < militaryHour[i]) {
+//     console.log(i);
+//     document.getElementById(i).classList.add("future");
+//   }
+
+
+//   // accessing key name according to id + repop. data from local storage
+//   if(localStorage.getItem(i)){
+//       document.getElementById(i).value = localStorage.getItem(i);
+//   }
+
+//   saveBtn[i].addEventListener("click",function(){
+//     var id = this.getAttribute("data-id");
+//     var text = document.getElementById(id).value;
+//      localStorage.setItem(id,text);
+//     console.log(text);
+// })
+// }
+
+
+//code4
     // Create time blocks for each hour
     // for (let hour = 9; hour <= 17; hour++) {
     //     createPlannerBlock(hour);
@@ -96,7 +175,7 @@ $("#time-block").append(row);
 
 
 
-
+//code5
 // $(document).ready(function () {
 
 // //     $("#currentDay").text(dayjs().format("MMMM D, YYYY"));
@@ -135,3 +214,10 @@ $("#time-block").append(row);
 //  $ ("#time-block") .append (row);
 
 // // )}; 
+
+
+//code
+
+// const currentDay = $("#currentDay");
+// var today = dayjs();
+// currentDay.text(today.format("D MM YYYY"));
